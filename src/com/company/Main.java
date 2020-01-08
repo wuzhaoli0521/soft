9e12f33ea71fa7c2d62c32ed99785cddcaaa7755
wuzl <wuzhaoli0521@gmail.com>
2020-01-08 17:48:59 +0800

    0108-login(view)

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Image img;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
